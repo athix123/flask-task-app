d8966e2a2f5a4a98337d81bfd4f63aaf89afdc53
athix123 <athizmubaroz2@gmail.com>
2019-09-28 22:29:14 +0700

    edit minnor

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 class Todo(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
